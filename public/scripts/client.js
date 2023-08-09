7bf63488f2be1a154a1556eefc06eb6527115dcc
Angela Zhou <a03zhou@gmail.com>
2023-08-09 03:58:15 -0400

    create renderTweets function and change test code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
+  
+  //test data
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
 const createTweetElement = function(tweetObj) {
   let userInfo = tweetObj.user;
@@ -26,26 +52,14 @@ const createTweetElement = function(tweetObj) {
   return $tweet;
 }
 
-// const $tweet = $(`<article class="tweet">test</article>`);
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-
-const $tweet = createTweetElement(tweetData);
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    $('.container').append(createTweetElement(tweet));
+  }
+};
 
 // Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('.container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+renderTweets(data);
+// console.log($tweet); // to see what it looks like
+ // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
